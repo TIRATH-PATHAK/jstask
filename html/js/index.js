96e8a6f2d77c9ce57d97c6547ef2c55f7278c3cf
TIRATH PATHAK <tirathp.wot2021@gmail.com>
2021-09-08 19:18:37 +0530

    Added

---
+++ html/js/index.js
@@ -0,0 +1,15 @@
+
+// var panels = document.getElementsByClassName('texts').querySelectorAll('.texts');
+// for (let i = 0; i < panels.length; i++) {
+//     panels[i].addEventListener('click',acc)
+// }
+
+function acc(number) {
+
+        
+        document.getElementById(number).style.display = 'block';
+    
+       // document.getElementById(number).style.display = 'none';
+    
+  
+}
